9b9679397c8f318e48e95e0e45c8235ad702589e
Patryk Ruta <90778187+Kakovs@users.noreply.github.com>
2022-08-07 20:32:16 +0200

    Slider (#2)
    * Desktop slider
    
    * Mobile slider

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,10 @@
+// Scrool header
+
 let scrollHeader = document.querySelector("header");
 
 const handleScroll = () => {
 
 
-    
     const scrooled = window.scrollY;
     if( scrooled > 40){
         scrollHeader.classList.add("smallHeader");
@@ -14,6 +15,7 @@ const handleScroll = () => {
 
 document.addEventListener("scroll", handleScroll);
 
+// Hamburger, turn on/off mobile menu
 
 let hamburger = document.querySelector(".hamburger");
 
@@ -30,3 +32,33 @@ hamburger.addEventListener("click", activateMenu);
 
 
 
+// slider
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+        disableOnHover: true,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    on: {
+        init() {
+          this.el.addEventListener('mouseenter', () => {
+            this.autoplay.stop();
+          });
+    
+          this.el.addEventListener('mouseleave', () => {
+            this.autoplay.start();
+          });
+        }
+      },
+
+  });
