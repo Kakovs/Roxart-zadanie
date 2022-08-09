6ade9785c7b6bc7c163b028f650f16e5ed7f22cd
Patryk Ruta <p4trykruta@gmail.com>
2022-08-09 14:20:28 +0200

    gallery desktop

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // Scrool header
 
-let scrollHeader = document.querySelector("header");
+const scrollHeader = document.querySelector("header");
 
 const handleScroll = () => {
 
@@ -8,25 +8,28 @@ const handleScroll = () => {
     const scrooled = window.scrollY;
     if( scrooled > 40){
         scrollHeader.classList.add("smallHeader");
-    }else{
-        scrollHeader.classList.remove("smallHeader");
+        return;
     }
+        scrollHeader.classList.remove("smallHeader");
+
 }
 
 document.addEventListener("scroll", handleScroll);
 
 // Hamburger, turn on/off mobile menu
 
-let hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger");
 
 const activateMenu = () =>{
 
     if (hamburger.classList.contains("active")) {
-        hamburger.classList.remove("active")
-    } else {
-        hamburger.classList.add("active")
+        hamburger.classList.remove("active");
+        return;
     }
+
+    hamburger.classList.add("active")
 }
+
 
 hamburger.addEventListener("click", activateMenu);
 
@@ -34,8 +37,7 @@ hamburger.addEventListener("click", activateMenu);
 
 // slider
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+const swiper = new Swiper('.homePageSlider', {
     direction: 'horizontal',
     loop: true,
     autoplay: {
@@ -43,8 +45,6 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
         disableOnHover: true,
     },
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -82,3 +82,32 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+
+// gallery carousel
+
+const swiperCarousel = new Swiper(".galleryCarousel", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    disableOnHover: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    init() {
+      this.el.addEventListener('mouseenter', () => {
+        this.autoplay.stop();
+      });
+
+      this.el.addEventListener('mouseleave', () => {
+        this.autoplay.start();
+      });
+    }
+  },
+});
